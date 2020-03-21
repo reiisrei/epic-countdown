a3e34f61b54deb2ac1c4769165638c637e37c566
Marjon Ramos <ramosmarjonf@gmail.com>
2020-03-21 20:31:02 +0800

    Added toastr

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,16 @@ Route::get('/', function () {
 
 Route::get('/', 'CountdownController@index');
 
+
 Route::get('epic/{epic}', 'CountdownController@show'); //show individual countdown
 
-Route::get('new-epic', 'CountdownController@create'); //show create page countdown
-Route::post('store-epic', 'CountdownController@store');
-Route::get('epic/{epic}/edit', 'CountdownController@edit');
-Route::post('epic/{epic}/update-epic', 'CountdownController@update');
-Route::get('epic/{epic}/delete', 'CountdownController@destroy');
+Route::get('new-epic', 'CountdownController@create')->name('create.countdown'); //show create page countdown
+Route::post('store-epic', 'CountdownController@store')->name('store.countdown');
+Route::get('epic/{epic}/edit', 'CountdownController@edit')->name('edit.countdown');
+Route::post('epic/{epic}/update-epic', 'CountdownController@update')->name('update.countdown');
+Route::get('epic/{epic}/delete', 'CountdownController@destroy')->name('delete.countdown');
 
-Route::get('epic/{epic}/complete', 'CountdownController@complete');
+Route::get('epic/{epic}/complete', 'CountdownController@complete')->name('complete.countdown');
 
 Route::get('test', 'CountdownController@test');
 
