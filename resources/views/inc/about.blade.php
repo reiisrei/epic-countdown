7cdce429295dbc3dc0176d5fe2852eca42b84800
Marjon Ramos <ramosmarjonf@gmail.com>
2020-04-02 19:18:09 +0800

    fixed about section

--- resources/views/inc/about.blade.php
+++ resources/views/inc/about.blade.php
@@ -16,18 +16,18 @@
       <!-- About Section Content -->
       <div class="row">
         <div class="col-lg-4 ml-auto">
-          <p class="lead">Freelancer is a free bootstrap theme created by Start Bootstrap. The download includes the complete source files including HTML, CSS, and JavaScript as well as optional SASS stylesheets for easy customization.</p>
+          <p class="lead">This website is made by Marjon Ramos as a  personal project. I used laravel to create this project. I hope to improve this project as time goes on. If you want to contribute to this project you can go to my <a href="https://github.com/reiisrei/epic-countdown" style="color:black">github</a> link. </p>
         </div>
         <div class="col-lg-4 mr-auto">
-          <p class="lead">You can create your own custom avatar for the masthead, change the icon in the dividers, and add your email address to the contact form to make it fully functional!</p>
+          <p class="lead">This website is for anyone who wants to create their own countdown of their favorite shows and upcoming movies.</p>
         </div>
       </div>
 
       <!-- About Section Button -->
       <div class="text-center mt-4">
-        <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/themes/freelancer/">
-          <i class="fas fa-download mr-2"></i>
-          Free Download!
+        <a class="btn btn-xl btn-outline-light" href="#">
+          <i class="fas fa-user-ninja mr-2"></i>
+          My Portfolio
         </a>
       </div>
 
