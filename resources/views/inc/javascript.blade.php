2458c6a423a197269835517d5e21ecd296cbe6ad
Marjon <ramosmarjonf@gmail.com>
2020-10-26 11:06:40 +0800

    Change asset( to secure_asset( so that it uses https instead of http

--- resources/views/inc/javascript.blade.php
+++ resources/views/inc/javascript.blade.php
@@ -2,28 +2,28 @@
 
 
     <!-- Bootstrap core JavaScript -->
-      <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-      <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+      <script src="{{ secure_asset('vendor/jquery/jquery.min.js') }}"></script>
+      <script src="{{ secure_asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
       <!-- Plugin JavaScript -->
-      <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+      <script src="{{ secure_asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
 
       <!-- Contact Form JavaScript -->
-      <script src="{{ asset('js/jqBootstrapValidation.js') }}"></script>
-      <script src="{{ asset('js/contact_me.js') }}"></script>
+      <script src="{{ secure_asset('js/jqBootstrapValidation.js') }}"></script>
+      <script src="{{ secure_asset('js/contact_me.js') }}"></script>
 
       <!-- Custom scripts for this template -->
-      <script src="{{ asset('js/freelancer.min.js') }}"></script>
-      <script src="{{asset('js/app.js')}}"></script>
+      <script src="{{ secure_asset('js/freelancer.min.js') }}"></script>
+      <script src="{{secure_asset('js/app.js')}}"></script>
 
       <script
       src="https://code.jquery.com/jquery-3.4.1.js"
       integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
       crossorigin="anonymous"></script>
 
-      <script type="text/javascript" src="{{ asset('js/countdown.js') }}"></script>
-      <script type="text/javascript" src="{{ asset('js/moment.js') }}"></script>
-      <script type="text/javascript" src="{{ asset('js/moment-timezone-with-data.js') }}"></script>
+      <script type="text/javascript" src="{{ secure_asset('js/countdown.js') }}"></script>
+      <script type="text/javascript" src="{{ secure_asset('js/moment.js') }}"></script>
+      <script type="text/javascript" src="{{ secure_asset('js/moment-timezone-with-data.js') }}"></script>
 
       <script type="text/javascript">
 
