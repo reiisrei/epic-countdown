02e79a091b2c9eaa7943b4b8b15af989a6151e7a
Marjon <ramosmarjonf@gmail.com>
2020-10-26 10:42:10 +0800

    fix schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        Schema::defaultStringLenght(191);
+        Schema::defaultStringLength(191);
     }
 
     /**
